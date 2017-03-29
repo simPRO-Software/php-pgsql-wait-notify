98c0473f51bc2865d2e169f795c395f245b41a6e
Rob Ormond <rob.ormond@simpro.co>
2017-03-29 13:19:05 +1000

    Initial commit

---
+++ config.h
@@ -0,0 +1,149 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Whether to build pgsql as dynamic module */
+#define COMPILE_DL_PGSQL 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Whether to build PostgreSQL support or not */
+#define HAVE_PGSQL 1
+
+/* Whether libpq is compiled with --enable-multibyte */
+#define HAVE_PGSQL_WITH_MULTIBYTE_SUPPORT 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PGTRANSACTIONSTATUS 1
+
+/* Whether to have pg_config.h */
+#define HAVE_PG_CONFIG_H 1
+
+/* PostgreSQL 8.1 or later */
+#define HAVE_PG_LO_CREATE 1
+
+/* PostgreSQL 8.4 or later */
+#define HAVE_PG_LO_IMPORT_WITH_OID 1
+
+/* PostgreSQL 7.0.x or later */
+#define HAVE_PQCLIENTENCODING 1
+
+/* Broken libpq under windows */
+#define HAVE_PQCMDTUPLES 1
+
+/* PostgreSQL 7.2.0 or later */
+#define HAVE_PQESCAPE 1
+
+/* PostgreSQL 9.0 or later */
+#define HAVE_PQESCAPELITERAL 1
+
+/* PostgreSQL 8.1.4 or later */
+#define HAVE_PQESCAPE_BYTEA_CONN 1
+
+/* PostgreSQL 8.1.4 or later */
+#define HAVE_PQESCAPE_CONN 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQEXECPARAMS 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQEXECPREPARED 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQFREEMEM 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQFTABLE 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQGETCOPYDATA 1
+
+/* Older PostgreSQL */
+#define HAVE_PQOIDVALUE 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQPARAMETERSTATUS 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQPREPARE 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQPROTOCOLVERSION 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQPUTCOPYDATA 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQPUTCOPYEND 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQRESULTERRORFIELD 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQSENDPREPARE 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQSENDQUERYPARAMS 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQSENDQUERYPREPARED 1
+
+/* PostgreSQL 7.4 or later */
+#define HAVE_PQSETERRORVERBOSITY 1
+
+/* PostgreSQL 7.0.x or later */
+#define HAVE_PQSETNONBLOCKING 1
+
+/* PostgreSQL 7.3.0 or later */
+#define HAVE_PQUNESCAPEBYTEA 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
